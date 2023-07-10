c10ad422f7421171887baba8a03a7e817324c001
Ada Gürsoy <50636585+ShinnAsukha@users.noreply.github.com>
2023-07-10 09:05:24 +0300

    Update starsteam.py

--- starsteam.py
+++ starsteam.py
@@ -25,7 +25,6 @@ if sys.platform == "linux" or sys.platform == "linux2":
 elif sys.platform == "win32":
     os.system("cls")
 
-asciibanner()
 print ("\033[1;32m")
 url = raw_input("          URL:  ").strip()
 print ("\033[1;m")
